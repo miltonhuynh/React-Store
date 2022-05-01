67cd3f22baa39ad0b8eaf180efdc9b3274dbd526
miltonhuynh <miltonhuynh@gmail.com>
2022-04-30 21:55:56 -0700

    Styled front page

--- app/src/Pages/ProductCard.js
+++ app/src/Pages/ProductCard.js
@@ -4,8 +4,8 @@ export default function ProductCard({image,title,price}) {
     console.log(image);
   return (
     <div id="Card_container">
-        <div>{title}</div>
-        <img id="card_images" src={image}></img>
+        <div id="Card_titles">{title}</div>
+        <img id="Card_images" src={image}></img>
         <div>${price}</div>
     </div>
   )
