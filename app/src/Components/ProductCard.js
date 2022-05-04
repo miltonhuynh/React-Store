ffe80e0b95d7cffbee68336e724be60fee5dc85d
miltonhuynh <miltonhuynh@gmail.com>
2022-05-04 15:53:27 -0700

    Router changes

--- app/src/Components/ProductCard.js
+++ app/src/Components/ProductCard.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function ProductCard({image,title,price}) {
   return (
+    <Link to="ProductDetails">
     <div id="Card_container">
         <div id="Card_titles">{title}</div>
         <img id="Card_images" src={image} alt="Shows item"></img>
         <div id="Card_prices">${price}</div>
     </div>
+    </Link>
   )
 }
