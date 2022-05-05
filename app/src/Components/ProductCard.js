5cc8a3af61bb9f334d5c257c834129fc37dd5cb7
miltonhuynh <miltonhuynh@gmail.com>
2022-05-04 17:09:31 -0700

    Added click function for cards

--- app/src/Components/ProductCard.js
+++ app/src/Components/ProductCard.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 
-export default function ProductCard({image,title,price}) {
+export default function ProductCard({image, title, price}) {
+  const clicked = () => {
+    alert('click works');
+  }
+
   return (
-    <Link to="ProductDetails">
-    <div id="Card_container">
+    <div id="Card_container" onClick={clicked}>
         <div id="Card_titles">{title}</div>
         <img id="Card_images" src={image} alt="Shows item"></img>
         <div id="Card_prices">${price}</div>
     </div>
-    </Link>
   )
 }
