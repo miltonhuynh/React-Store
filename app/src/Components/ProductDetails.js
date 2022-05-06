a523899fff6ba4bf87035da908d79433e6c8d235
miltonhuynh <miltonhuynh@gmail.com>
2022-05-05 21:32:36 -0700

    Added form to product details page to select quantity

--- app/src/Components/ProductDetails.js
+++ app/src/Components/ProductDetails.js
@@ -4,6 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 
 export default function ProductDetails() {
   const [Item, setItem] = useState(null);
+  const [Quantity, setQuantity] = useState();
   let params = useParams();
 
   useEffect(() => {
@@ -13,7 +14,12 @@ export default function ProductDetails() {
           setItem(data);
       }
       fetchData();        
-  }, []);
+  }, [params.id]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(Quantity);
+}
   
   // Check if fetch has completed successfully before sending data to children
   if(Item != null) {
@@ -21,11 +27,28 @@ export default function ProductDetails() {
       <>
       <Link to="/React-Store">Back</Link>
         <div id="Details_container">
-          <img src={Item.image} id="Details_image"></img>
+          <img src={Item.image} id="Details_image" alt="About item"></img>
           <div id="Details_title">{Item.title}</div>
           <div id="Details_price">${Item.price}</div>
           <div id="Details_price">Category: {Item.category}</div>
           <div id="Details_price">Description: {Item.description}</div>
+
+          <form onSubmit={handleSubmit} id="Quantity_form">
+            <label htmlFor="quantity">Quantity:</label>
+              <select id="quantity" name="quantity" onChange = {(event) => setQuantity(event.target.value)}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+              <button type="submit">Add to cart</button>
+            </form>
         </div>
       </>
     )
--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductPage from './Components/ProductPage';
 import ProductDetails from './Components/ProductDetails';
 import Cart from './Components/Cart';
