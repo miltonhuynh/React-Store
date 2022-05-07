7047fe00141af523bbbd19939c5e4aa828438635
miltonhuynh <miltonhuynh@gmail.com>
2022-05-06 17:56:47 -0700

    Started function to delete from cart

--- app/src/Components/CartItem.js
+++ app/src/Components/CartItem.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import {React, useContext} from 'react'
+import { CartContext } from '../App';
 
-export default function CartItem({title, quantity,price}) {
+
+export default function CartItem({title, quantity, price}) {
+
+  const {Cart, setCart} = useContext(CartContext);
+
+  //var filtered = someArray.filter(function(el) { return el.Name != "Kristian"; });
+  function remove_item() {
+    let removed = Cart.filter(function(remove) { return remove.title === title})
+    setCart(removed);
+  }
   return (
     <div id="Cart_item">
         <div>Title: {title}</div>
         <div>Quantity: {quantity}</div>
         <div>Price each: ${price}</div>
+        <button onClick={() => remove_item()}>Delete</button>
     </div>
     )
   }
