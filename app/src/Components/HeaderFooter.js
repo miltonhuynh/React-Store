5cfc1eb7b1edd9ebb94b51f3fea697764bf8a6fb
miltonhuynh <miltonhuynh@gmail.com>
2022-05-09 19:59:35 -0700

    Added box shadow to cards when not hovered on

--- app/src/Components/HeaderFooter.js
+++ app/src/Components/HeaderFooter.js
@@ -4,7 +4,6 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
-import { faMobileScreen } from '@fortawesome/free-solid-svg-icons'
 import { ReactComponent as Githubicon } from '../github-brands.svg';
 
 export default function HeaderFooter(props) {
@@ -24,11 +23,6 @@ export default function HeaderFooter(props) {
     <>
       <div id="Header">
         <div id="Brand" onClick={goHome}>The React Store</div>
-        <div>
-          <a href="https://miltonhuynh.github.io/Mobile-React-Store/">
-            <FontAwesomeIcon icon={faMobileScreen} id="Mobile-icon"/>
-          </a>
-        </div>
         <div id="Github-icon"><a href="https://github.com/miltonhuynh/React-Store"><Githubicon/></a></div>
         <div id="Navbar">
           <div className="Nav-icon" onClick={goHome}><FontAwesomeIcon icon={faHome} /></div>
